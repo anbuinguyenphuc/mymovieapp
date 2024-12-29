327427212f52c320c4e451ce2dd7935ef53fc576
An Bui <annguyenphuc@netwealth.com.au>
2024-12-29 08:49:05 +0700

    move api key to env file

--- src/api/api-action.ts
+++ src/api/api-action.ts
@@ -1,1 +1,3 @@
-export const API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMDUwZmYxZWEwNjEwYmZlNDJkMGJiMzEyZWUxNmRiYSIsIm5iZiI6MTczNTI3MDE3NC4xOTkwMDAxLCJzdWIiOiI2NzZlMWYxZTc2OTg1MmYyOTAxMjlkZDUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.-aFhPbLWbDyO_JTeURYXRdX9tTYug-HBAalQhmwXRQI'
+import Config from 'react-native-config';
+export const API_KEY = Config.API_KEY;
+
