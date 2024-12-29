83cc189be3c676a9b5f4f9ad412a941b22d826d3
An Bui <annguyenphuc@netwealth.com.au>
2024-12-29 16:42:55 +0700

    add todo note

--- App.tsx
+++ App.tsx
@@ -20,11 +20,29 @@ import {initSdk} from 'mymoviesdk';
 import {API_KEY} from './src/api/key';
 import {setJSExceptionHandler} from 'react-native-exception-handler';
 import {handleError} from './src/hook/useHandleError';
+
+/**
+ * //TODO
+ * WHAT COULD BE DONE BETTER IN THIS PROJECT IF IT IS A REAL PRODUCT
+ * 1. Caching movies list to reduce loading time and reduce traffic for Backend Services by using mem-cache technic and react-native-mmkv storage.
+ * 2. Adding Redux (thunk) to support state management at scale better, for now I'm think it's good enough to use React Hook.
+ * 3. Adding proguard to protect reverse engineering.
+ * 4. Adding feature searching movie by keywords because currently, the offical api that suppot searching by keywords is deprecated and don't look optimized for searching flow. 
+ * 5. Optimizing movie list performance by calculating internet quality to swith bettween hight and low quality images. 
+ * 6. Optimizing movie detail screen by lazy loading Review section (only start requesting reviews when user scroll near to bottom).
+ * 7. Covering app with test cases by using Jest or Detox.
+ * 8. Have better UI design.
+ * 9. Consider switching to flashlist to improve list performance at scale.
+ */
+
 //setJSExceptionHandler to handle JS exceptions
 setJSExceptionHandler((error, isFatal) => {
   handleError(error);
 }, true);
+
+//init Movie SDK
 initSdk({apiKey: API_KEY});
+
 const Stack = createNativeStackNavigator();
 const App = () => {
   return (
